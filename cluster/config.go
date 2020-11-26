5cab1f32dd0278650794c0810c427f0ba6fd8182
yufeifly <yufei.xiong@qq.com>
2020-11-26 20:05:37 +0800

    fix relative path problem, but the solution is not elegant

--- cluster/config.go
+++ cluster/config.go
@@ -6,26 +6,39 @@ import (
 	"github.com/yufeifly/migrator/model"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 var DefaultCluster model.Cluster
 
 func init() {
-	LoadClusterConfig()
+	err := LoadClusterConfig()
+	if err != nil {
+		logrus.Panicf("cluster.init LoadClusterConfig failed, err: %v", err)
+	}
 }
 
-func LoadClusterConfig() {
-	jsonFile, err := os.Open("cluster.json")
+func LoadClusterConfig() error {
+	// fixme using GetWd function is not elegant
+	dir, err := os.Getwd()
 	if err != nil {
-		logrus.Panicf("cluster.LoadClusterConfig open file failed, err: %v", err)
+		return err
+	}
+	configFilePath := filepath.Join(dir, "cluster/cluster.json")
+	jsonFile, err := os.Open(configFilePath)
+	if err != nil {
+		logrus.Errorf("cluster.LoadClusterConfig open file failed, err: %v", err)
+		return err
 	}
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	err = json.Unmarshal(byteValue, &DefaultCluster)
 	if err != nil {
-		logrus.Panicf("cluster.LoadClusterConfig Unmarshal failed, err: %v", err)
+		logrus.Errorf("cluster.LoadClusterConfig Unmarshal failed, err: %v", err)
+		return err
 	}
 	//logrus.Infof("the cluster: %v", DefaultCluster)
+	return nil
 }
 
 func Cluster() *model.Cluster {
